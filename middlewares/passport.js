95382789fec6d38019e32df97fcbd46dddd9e1b7
Jorge Centeno <george@Macintosh.local>
2023-02-16 12:32:13 +0100

    passport

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -1,5 +1,5 @@
 import * as usersServices  from '../services/usersServices.js'
-import {Strategy, ExtractJwt} from "passport-local"
+import {Strategy, ExtractJwt} from "passport-jwt"
 import dotenv from 'dotenv'
 dotenv.config()
 
