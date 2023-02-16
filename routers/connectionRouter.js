f850317d81cec67cd63c92463760b5a0f0bdbbde
Jorge Centeno <george@Macintosh.local>
2023-02-16 10:10:19 +0100

    first commit

---
+++ routers/connectionRouter.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import passport from "passport";
+import * as connectionController from '../controllers/connectionController.js'
+
+const connectionRouter = Router()
+connectionRouter.post('/connectToCreditor/:email',
+    passport.authenticate('jwt', {session:false}), 
+    connectionController.connectToCreditor)
+
+connectionRouter.patch('/disconnectToCreditor/:email',
+    passport.authenticate('jwt', {session:false}), 
+    connectionController.disconnectToCreditor)
+
+
+connectionRouter.get('/getMyDebtors/', 
+    passport.authenticate("jwt", { session: false }), 
+    connectionController.getMyDebtors)
+
+connectionRouter.get('/getMyCreditors/', 
+    passport.authenticate("jwt", { session: false }), 
+    connectionController.getMyCreditors)
+
+    connectionRouter.post('/addDebt/:idConnection',
+    passport.authenticate("jwt", { session: false }),
+    connectionController.addDebt )
+
+export default connectionRouter
